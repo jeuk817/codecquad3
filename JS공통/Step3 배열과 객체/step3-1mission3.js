59e3917a4abd3d63ca117a19351a4364fc18d762
Ryujeuk <jeuk817@gmail.com>
2019-04-13 22:43:26 +0900

    재귀로 구현시도
    initialValue를 리턴값으로 넣어주는 재귀함수를 만들었다.(좀 더 실험 필요)

--- JS공통/Step3 배열과 객체/step3-1mission3.js
+++ JS공통/Step3 배열과 객체/step3-1mission3.js
@@ -12,8 +12,12 @@
 // const result = myReduce(arr, (next,prev) => {...}, []);
 
 const myReduce = (arr, callback, initialValue) => {
-    var result = arr.forEach(callback(initialValue, prev));
-    console.log(result);
+    var result = callback(initialValue, arr.shift());
+    if(arr.length === 0){
+        console.log(result,'=======');
+        return result;
+    }
+    myReduce(arr, callback, result);
 }
 
 const result = myReduce([1,2,3,4,5], (next,prev) => {return next +prev}, [10]);
